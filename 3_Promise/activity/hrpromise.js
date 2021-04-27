577d9d34464dcb0e3ebc4b4d77e85845b255b87f
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-04-27 21:06:52 +0530

    waitandclick

--- 3_Promise/activity/hrpromise.js
+++ 3_Promise/activity/hrpromise.js
@@ -1,59 +1,58 @@
-let puppeteer = require("puppeteer");
-
-let fs = require("fs");
+let fs=require("fs");
+let puppeteer=require("puppeteer");
+let link="https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login";
 let gtab;
-console.log("Before");
-let browserPromise = puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    args: ["--start-maximized",]
+// node hrpromise
+function waitAndClickk(selector) {
+    return new Promise(function (resolve, reject) {
+        let selectorWaitPromise = gtab.waitForSelector(selector,
+            { visible: true });
+        selectorWaitPromise
+            .then(function () {
+                let selectorClickPromise = gtab.click(selector);
+                return selectorClickPromise;
+            }).then(function () {
+                resolve();
+            }).catch(function () {
+                reject(err);
+            })
+    })
+}
+let nbrowser=puppeteer.launch({headless:false, args:["--start-maximized"]});
+nbrowser.then(function(browser){
+    let ntab=browser.newPage();
+    return ntab;
+}).then(function(ntab){
+    gtab=ntab;
+    let hrank=gtab.goto(link);
+    return hrank;
+}).then(function(){
+    let username=gtab.type("[name='username']",'jonenag175@sumwan.com',{delay:10})
+    return username;
+}).then(function(){
+    let password=gtab.type("[name='password']",'jonenag175@sumwan.com',{delay:10})
+    return password;
+}).then(function(){
+    let login=gtab.click("button[data-analytics='LoginPassword']")
+    return login
+}).then(function () {
+    let clickIPKIt = waitandclick
+        (".card-content h3[title='Interview Preparation Kit']");
+    return clickIPKIt;
+}).then(function () {
+    let warmupClick =
+    waitAndClick("a[data-attr1='warmup']");
+    return warmupClick;
 })
-let username='jonenag175@sumwan.com';
-let password='jonenag175@sumwan.com'
-// /pages
-browserPromise
-    .then(function (browserInstance) {
-        let newTabPromise = browserInstance.newPage();
-        return newTabPromise;
-    })
-    .then(function (newTab) {
-        // console.log(newTab);
-        let loginPageWillBeopenedPromise = newTab.goto("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
-        gtab = newTab;
-        return loginPageWillBeopenedPromise;
-    })
-    .then(function () {
-        // console.log("login Page opened");
-        let emailWillBeTypedPromise = gtab.type("#input-1", email, { delay: 10 });
-        return emailWillBeTypedPromise;
-    }).then(function () {
-        let passwordWillBeTypedPromise = gtab.type("#input-2", password, { delay: 10 });
-        return passwordWillBeTypedPromise;
-    }).then(function () {
-        let loginPageWillBeClickedpromise = gtab.
-            click("button[data-analytics='LoginPassword']");
-        return loginPageWillBeClickedpromise;
-    }).then(function () {
-        let clickIPKIt = waitAndClick
-            (".card-content h3[title='Interview Preparation Kit']");
-        return clickIPKIt;
-    }).then(function () {
-        let warmupClick =
-            waitAndClick("a[data-attr1='warmup']");
-        return warmupClick;
-    })
-    function waitAndClick(selector) {
-        return new Promise(function (resolve, reject) {
-            let selectorWaitPromise = gtab.waitForSelector(selector,
-                { visible: true });
-            selectorWaitPromise
-                .then(function () {
-                    let selectorClickPromise = gtab.click(selector);
-                    return selectorClickPromise;
-                }).then(function () {
-                    resolve();
-                }).catch(function () {
-                    reject(err);
-                })
+
+function waitAndClick(selector){
+    return new Promise(function(resolve, reject){
+        let wwait=gtab.waitForSelector(selector,{visible:true});
+        wwait.then(function(){
+            cclick=gtab.click(selector);
+            return cclick;
+        }).then(function(){
+            resolve();
         })
-    }
+    })
+}
