df8dd273adae2283def045f964be006192d4c3a4
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-05-12 02:51:40 +0530

    box and four colours created

--- 5_Browser/activity/script.js
+++ 5_Browser/activity/script.js
@@ -1,7 +1,7 @@
-let color1=document.querySelector(".color1")
-let color2=document.querySelector(".color2")
-let color3=document.querySelector(".color3")
-let color4=document.querySelector(".color4")
+let color1=document.querySelector(".first")
+let color2=document.querySelector(".second")
+let color3=document.querySelector(".third")
+let color4=document.querySelector(".fourth")
 let outer=document.querySelector(".outer2")
 color1.addEventListener("click",function(data){
     outer.style.backgroundColor="Red";
@@ -17,11 +17,11 @@ color4.addEventListener("click",function(data){
 })
 let add=document.querySelector(".fas.fa-plus");
 add.addEventListener("click",function(data){
-    let nb=document.createElement("div");
-    nb.setAttribute("class","todo-box");
-    nb.style.background="Red";
-    nb.style.width="25%";
-    nb.style.height="40%";
-    outer.appendChild(nb);
+    // let nb=document.createElement("div");
+    // nb.setAttribute("class","todo-box");
+    // nb.style.background="Red";
+    // nb.style.width="25%";
+    // nb.style.height="40%";
+    // outer.appendChild(nb);
 })
 
