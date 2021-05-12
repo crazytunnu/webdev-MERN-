ceeddb83148af190a598c7c0d072b94b4aff7540
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-05-12 07:23:24 +0530

    completed but inefficient and less attractive

--- 5_Browser/activity/script.js
+++ 5_Browser/activity/script.js
@@ -2,24 +2,28 @@ let color1=document.querySelector(".first")
 let color2=document.querySelector(".second")
 let color3=document.querySelector(".third")
 let color4=document.querySelector(".fourth")
-let outer=document.querySelector(".outer2")
+let outer=document.body;
+let outer2=document.querySelector(".outer2")
 let add=document.querySelector(".fas.fa-plus");
+
+let sc;
+
 color1.addEventListener("click",function(data){
-    outer.style.backgroundColor="Red";
+    outer2.style.backgroundColor="Red";
 })
 color2.addEventListener("click",function(data){
-    outer.style.backgroundColor="blue";
+    outer2.style.backgroundColor="blue";
 })
 color3.addEventListener("click",function(data){
-    outer.style.backgroundColor="GreEn";
+    outer2.style.backgroundColor="GreEn";
 })
 color4.addEventListener("click",function(data){
-    outer.style.backgroundColor="YELLOW";
+    outer2.style.backgroundColor="YELLOW";
 })
 add.addEventListener("click",function(data){
-    let nb=document.createElement("div");
-    nb.setAttribute("class","modal");
-    nb.innerHTML=`<div class="input-box">
+    let nbg=document.createElement("div");
+    nbg.setAttribute("class","modal");
+    nbg.innerHTML=`<div class="input-box">
     <textarea class="text-box">Enter your text here</textarea>
     </div>
     <div class="colorV">
@@ -28,6 +32,69 @@ add.addEventListener("click",function(data){
     <div class="filter third"></div>
     <div class="filter fourth"></div>
 </div>`;
-    outer.appendChild(nb);
+
+    outer.appendChild(nbg);
+    let co1=document.querySelector(".filter.first")
+let co2=document.querySelector(".filter.second")
+let co3=document.querySelector(".filter.third")
+let co4=document.querySelector(".filter.fourth")
+co1.addEventListener("click",function(data){
+    removeclass();
+    co1.classList.add("border");
+    sc="red";
+   
 })
+co4.addEventListener("click",function(data){
+    removeclass();
+    co4.classList.add("border");
+    sc="yellow";
+})
+co2.addEventListener("click",function(data){
+    removeclass();
+    co2.classList.add("border");
+    sc="blue";
+})
+co3.addEventListener("click",function(data){
+    removeclass();
+    co3.classList.add("border");
+    sc="green";
+})
+let tb=document.querySelector(".text-box");
+tb.addEventListener("click",function(data){
+    tb.innerHTML="";
+    })
+    tb.addEventListener("keydown",function(data){
+        if(data.key=="Enter")
+        {
+        let nb=document.createElement("div");
+    nb.setAttribute("class","todo");
+    nb.innerHTML=`<div class="td-head"></div>
+    <div class="tdEID">#ExampleID</div>`;
+    outer2.appendChild(nb);
+    let tex=document.querySelector("td-text");
+    let xx=document.querySelector(".text-box");
+    let parent=document.querySelectorAll(".todo");
+    parent=parent[parent.length-1];
+    let child=document.createElement("div");
+    child.setAttribute("class","td-text");
+    child.innerText=xx.value;
+    xx.value="";
+    parent.appendChild(child);
+    let la=document.querySelectorAll(".td-head");
+    la=la[la.length-1];
+    la.style.backgroundColor=sc;
+    nbg.remove();
+        }
+    })
+})
+function removeclass()
+{
+    
+    let filter=document.querySelectorAll(".filter");
+    for(let i=0;i<filter.length;i++)
+    {
+        filter[i].classList.remove("border");
+    }
+}
+
 
