c8dfbde01c4cf87177f4c13ac6f52d41078d82e0
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-05-16 05:49:14 +0530

    Storage Setting and getting done

--- 5_Browser/todo_jira/script.js
+++ 5_Browser/todo_jira/script.js
@@ -1,5 +1,20 @@
 let input=document.querySelector(".input-box")
 let ul=document.querySelector(".task-list");
+let arr=[];
+if(localStorage.getItem("allTask"))
+{
+    arr=JSON.parse(localStorage.getItem("allTask"))
+    for(let i=0;i<arr.length;i++)
+    {
+        let li=document.createElement("li");
+        li.innerText=arr[i];
+        li.addEventListener("dblclick",function(data){
+            li.remove();
+        })
+        li.setAttribute("class","task");
+        ul.appendChild(li);
+    }
+}
 input.addEventListener("keydown",function(data)
 {
     let kk=data.key;
@@ -9,11 +24,15 @@ input.addEventListener("keydown",function(data)
         let li=document.createElement("li");
         li.innerText=vv;
         li.addEventListener("dblclick",function(data){
+
             li.remove();
         })
         li.setAttribute("class","task");
         ul.appendChild(li);
         erasetext();
+        arr.push(vv);
+        let strArr=JSON.stringify(arr);
+        localStorage.setItem("allTask",strArr);
     }
     else if(data.key=="Backspace")
     {
