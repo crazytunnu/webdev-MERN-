2d3dd4c93ac19f30225809f475b3155e0ec49a33
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-05-17 07:20:01 +0530

    Selection of sheet done

--- 6_Excel/activity/script.js
+++ 6_Excel/activity/script.js
@@ -1,9 +1,8 @@
 let add_sheet_container=document.querySelector(".add-sheet_container");
 let sheets_list=document.querySelector(".sheets-list");
 let firstSheet=document.querySelector(".sheet")
-firstSheet.addEventListener("click")
+firstSheet.addEventListener("click",selected_sheet)
 add_sheet_container.addEventListener("click",addSheet)
-
 function addSheet(e)
 {
     let allsheet=document.querySelectorAll(".sheet");
@@ -15,4 +14,13 @@ function addSheet(e)
     sheet.setAttribute("sheetIdx",id+1);
     sheet.innerText=`Sheet ${id+2}`
     sheets_list.appendChild(sheet);
+    sheet.addEventListener("click",selected_sheet)
+}
+function selected_sheet(e)
+{
+    let sheet=document.querySelectorAll(".sheet");
+    sheet.forEach(function(sheet){
+        sheet.classList.remove("active-sheet")
+    })
+    e.currentTarget.classList.add("active-sheet");
 }
