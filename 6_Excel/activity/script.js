13686b51d42b85476beefb87bb7250f863c17c1b
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-05-17 07:00:00 +0530

    Till Creation of sheets with index

---
+++ 6_Excel/activity/script.js
@@ -0,0 +1,18 @@
+let add_sheet_container=document.querySelector(".add-sheet_container");
+let sheets_list=document.querySelector(".sheets-list");
+let firstSheet=document.querySelector(".sheet")
+firstSheet.addEventListener("click")
+add_sheet_container.addEventListener("click",addSheet)
+
+function addSheet(e)
+{
+    let allsheet=document.querySelectorAll(".sheet");
+    allsheet=allsheet[allsheet.length-1];
+    let id=allsheet.getAttribute("sheetIdx");
+    id=Number(id);
+    let sheet=document.createElement("div");
+    sheet.setAttribute("class","sheet");
+    sheet.setAttribute("sheetIdx",id+1);
+    sheet.innerText=`Sheet ${id+2}`
+    sheets_list.appendChild(sheet);
+}
