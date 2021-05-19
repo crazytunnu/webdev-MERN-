769816cd8b1d2fc36cb2a73c410ed401bcc463a8
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-05-19 06:54:48 +0530

    Alignment completed

--- 6_Excel/activity/script.js
+++ 6_Excel/activity/script.js
@@ -2,6 +2,12 @@ let add_sheet_container=document.querySelector(".add-sheet_container");
 let sheets_list=document.querySelector(".sheets-list");
 let firstSheet=document.querySelector(".sheet")
 let Allcells=document.querySelectorAll(".grid .col");
+let alignBtn=document.querySelectorAll(".alb")
+let adbox=document.querySelector(".address-box");
+
+
+
+
 firstSheet.addEventListener("click",selected_sheet)
 add_sheet_container.addEventListener("click",addSheet)
 function addSheet(e)
@@ -26,18 +32,40 @@ function selected_sheet(e)
     e.currentTarget.classList.add("active-sheet");
 }
 
-
-
-
 for(let i=0;i<Allcells.length;i++)
 {
     Allcells[i].addEventListener("click",function(e){
         let row=Allcells[i].getAttribute("rid");
         let col=Allcells[i].getAttribute("cid");
         let address=row+col;
-        let adbox=document.querySelector(".address-box");
+        
         adbox.value=address;
 
     })
 }
 Allcells[0].click();
+for(let i=0;i<3;i++)
+{
+    alignBtn[i].addEventListener("click",alignFuntion)
+}
+function alignFuntion(data)
+{
+    let alignment=data.currentTarget.value;
+    let xx=adbox.value;
+    let row=xx.charAt(0);
+    let col=Number(xx.slice(1));
+    let box=document.querySelector(`[rid='${row}'][cid='${col}']`);
+   
+    if(data.currentTarget.value=="L")
+    {
+        box.style.textAlign="left"
+    }
+    else if(data.currentTarget.value=="C")
+    {
+        box.style.textAlign="center"
+    }
+    else if(data.currentTarget.value=="R")
+    {
+        box.style.textAlign="right"
+    }
+}
