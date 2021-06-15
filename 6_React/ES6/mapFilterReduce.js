3c27f02c06c2e2507d34998da402e6613d0d03a1
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-06-15 22:16:15 +0530

    mapfilterreduce

--- 6_React/ES6/mapFilterReduce.js
+++ 6_React/ES6/mapFilterReduce.js
@@ -1,7 +1,27 @@
-function square(i)
+// function square(i)
+// {
+//     return i*i;
+// }
+// let arr=[1,2,3,4,5]
+// let dbarr=arr.map(square);
+// console.log(dbarr)
+//-------------------------------------------Filter
+// function lengthGreater(s)
+// {
+//     if(s.length>=6)return true;
+//     else return false;
+// }
+// let arr=["sdkjlas","sajdl","lkadsjl","sadhkjasdk","dkd"];
+// let sarr=arr.filter(lengthGreater);
+// console.log(sarr);
+
+
+
+//==============================Reduce
+function product(acc,cval)
 {
-    return i*i;
+    return acc*cval;
 }
-let arr=[1,2,3,4,5]
-let dbarr=arr.map(square);
-console.log(dbarr)
+const arr=[1,2,3,4,5];
+const ans=arr.reduce(product);
+console.log(ans);
