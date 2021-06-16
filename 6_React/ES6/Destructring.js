85db12d52b851f6ddea870a0b2dd72b6daaf688a
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-06-16 22:08:41 +0530

    Destructring array

---
+++ 6_React/ES6/Destructring.js
@@ -0,0 +1,33 @@
+let message=["Hello","I","am","Arshad"];
+// let greet=message[0];
+// let name=message[3];
+// let verb=message[2];
+// let pronoun=message[1];
+// let [greet,pronoun,verb,name]=message;
+// console.log(greet,name,verb,pronoun);
+
+
+
+//skipping certain index;
+// let [greet,,verb,name]=message;
+// console.log(greet,verb,name);
+
+
+
+// using more than elemets in array;
+// let arr=["Arshad","22"];
+// let [fname,age,address]=arr;
+// here address is on extra index therefore it is assigned undefined
+// console.log(fname,age,address);
+//fname=Arshad
+//age=22;
+//address=undefined
+
+
+
+
+// let a=10;
+// let b=20;
+// //swap it
+// [a,b]=[b,a];
+// console.log(a,b);
