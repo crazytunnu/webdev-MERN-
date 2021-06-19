276302a47ba047a39ac5131d90727e04689960d8
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-06-19 21:32:47 +0530

    destructuring objects

---
+++ 6_React/ES6/DestructringObjext.js
@@ -0,0 +1,24 @@
+let person={name:'Arshad',age:"22",job:'SWE'}
+
+
+
+// let name=person.name;
+// let job=person['job'];
+// console.log(name,job);
+
+
+
+
+// let {name,job,xyz}=person;
+// console.log(name,job,xyz);/////////////////////xyz will be undefined////////////////////////
+
+
+
+
+// let {name:naam,age:umar}=person
+// console.log(naam,umar);
+
+
+/////////////////////////////////////////////DEfault Value/////////////////////////////////////////
+let{name,xyz="Default",age="100"}=person;//not undefined in this case
+console.log(name,xyz,age);
