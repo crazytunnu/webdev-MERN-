fe5778d8797aabcf15e9923ab0c06d1819da50d9
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-06-29 22:08:43 +0530

    click button working

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,18 +2,37 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
-    tasks=[{id:1,txt:"first"},{id:2,txt:"second"},{id:3,txt:"third"}]
+    constructor(prop){
+        super(prop);
+        this.state={
+            tasks:[{id:1,txt:"first"},{id:2,txt:"second"},{id:3,txt:"third"}],
+            currTask:""
+        }
+    }
+    handleChange=(e)=>{
+        let val=e.target.value;
+        this.setState({currTask:val})
+    }
+    handleClick=()=>
+    {
+        let na=[...this.state.tasks,{id:this.state.tasks.length+1,txt:this.state.currTask}];
+        this.setState({
+            tasks:na,
+            currTask:""
+        })
+    }
     render() {
+        // console.log("rendered");
         return (
             <div>
                 <div className="input-container">
-                <input type='text'></input>
-                <button>Add</button>
+                <input onChange={this.handleChange} value={this.state.currTask} type='text'></input>
+                <button onClick={this.handleClick}>Add</button>
                 </div>
             <div className="class-list">
                 <ul>
                     {
-                        this.tasks.map(task=>(
+                        this.state.tasks.map(task=>(
                             <li>
                                 <h1>{task.txt}</h1>
                                 <button>Delete</button>
