e203c91a23e1ade0858ad115963fb84fd78634c6
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-07-04 00:21:29 +0530

    few error left

--- 6_React/movies/src/Component/Movies.js
+++ 6_React/movies/src/Component/Movies.js
@@ -9,7 +9,7 @@ export default class Movies extends Component {
     }
     onDelete=(id)=>{
         let narr=this.state.arr.filter(obj=>{
-            return id!=obj._id
+            return id!==obj._id
             console.log(id);
         })
         this.setState({arr:narr});
@@ -17,18 +17,64 @@ export default class Movies extends Component {
     render() {
         let arr=this.state.arr;
         return (
-            <div>
-                {
-                    arr.map(obj=>(
-                        <div className="movies-item" key={obj._id}>
-                            <span>{obj.title}</span>
-                            <span>{obj.genre.name}</span>
-                            <span>{obj.numberInStock}</span>
-                            <span>{obj.dailyRentalRate}</span>
-                            <button onClick={()=>this.onDelete(obj._id)}>delete</button>
-                        </div>
-                    ))
-                }
+            <div className='row'>
+                <div className='col-3'>Hello</div>
+                <div className='col-9'></div>
+                <input type='text'></input>
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+                <table class='table'>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">title</th>
+                <th scope="col">genre</th>
+                <th scope="col">stock</th>
+                <th scope="col">rate</th>
+            </tr>
+        </thead>        
+        <tbody>
+            {
+                arr.map(obj=>(
+                    <tr scope="row" key={obj._id}>
+                        <td></td>
+                        <td>{obj.title}</td>
+                        <td>{obj.genre.name}</td>
+                        <td>{obj.numberInStock}</td>
+                        <td>{obj.dialyRentalRate}</td>
+                        <td><button type="button" class="btn btn-danger" onClick={()=>this.onDelete(obj._id)}>Delet</button></td>
+                    </tr>
+                ))
+            }
+        </tbody>
+        </table>
             </div>
         )
     }
