4018f5fbb45ae59c96a37407d5010f3226f9af24
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-07-08 23:13:48 +0530

    Prop passing in route

--- 6_React/movies/src/App.js
+++ 6_React/movies/src/App.js
@@ -1,17 +1,20 @@
 import Movies from './Component/Movies'
-import about from './Component/about'
+import About from './Component/about'
 import home from './Component/home'
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 function App() {
   return (
     <>
     <BrowserRouter>
+    <Switch>
     <Route exact path="/" component={home}/>
-    <Route path="/aboutaaaaa" component={about}/>
+
+    <Route path='/about' render={(props)=>{ return <About {...props} isAuth={true} />}}/>
     <Route exact path="/movies" component={Movies}/>
+    {/* <Route exact path="/about" component={about}/> */}
     
     
-    
+    </Switch>
     </BrowserRouter>
     </>
   );
--- 6_React/movies/src/Component/about.js
+++ 6_React/movies/src/Component/about.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function about() {
+function about(props) {
+    console.log(props);
     return (
         <div>
             <h1>About</h1>
