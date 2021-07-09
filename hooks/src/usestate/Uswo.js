82da767c5403bfe4bfb0b6a6f7d78a96f5d75e36
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-07-09 23:21:18 +0530

    completed till object

--- hooks/src/usestate/Uswo.js
+++ hooks/src/usestate/Uswo.js
@@ -1,10 +1,24 @@
 import React,{useState} from 'react'
 
 export default function Uswo() {
+    const [msg,setMsg]=useState("");
+    const [obj,setObj]=useState({name:"",age:1})
+    const handleInput=(e)=>{
+        const x=e.target.value;
+        setMsg(x);
+    }
+    const handleObject=(e)=>{
+        let str=e.target.value;
+        let tObj={...obj,name:str}
+        setObj(tObj);
+    }
     return (
         <div>
-            
+            <input value={msg} onChange={handleInput} placeholder="Enter Your text here"></input>
+            <h1>{msg}</h1>
+            <input value={obj.name} onChange={handleObject} placeholder="Change in Object"></input>
+            <h1>{obj.name}</h1>
         </div>
     )
 }
-export default Uswo;
+// export default Uswo;
