a260bbc1f3f0fecb5bfe705ad285503cf16d63b9
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-07-13 22:59:04 +0530

    installed firebase

---
+++ 6_React/Demo_Firebase/src/component/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore'
+firebase.initialize(
+{
+    apiKey: "AIzaSyD4ZLGw_nGEg3aTNSY_evuYg2Ukq2h3N3U",
+    authDomain: "class-demo-6934a.firebaseapp.com",
+    projectId: "class-demo-6934a",
+    storageBucket: "class-demo-6934a.appspot.com",
+    messagingSenderId: "668960450659",
+    appId: "1:668960450659:web:c60dafa017d6f3fe5cf056"
+  })
