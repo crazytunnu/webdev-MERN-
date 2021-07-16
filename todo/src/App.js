c312ec990292579258762255f26f791336661f30
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-07-16 22:53:40 +0530

    q4 solved

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,22 @@
 import Hooksdemo from './Hooksdemo';
 import Q1 from './components/Q1';
 import Q2 from './components/Q2';
-import Q3 from './components/Q3';
+import Q4 from './components/Q4';
+// import Q5 from './components/Q5';
+// import Q6 from './components/Q6';
+// import Q7 from './components/Q7';
+// import Q8 from './components/Q8';
+// import Q9 from './components/Q9';
+// import Q10 from './components/Q10';
+
 function App() {
   return (
     <>
     {/* <Hooksdemo/> */}
     {/* <Q1/> */}
     {/* <Q2/> */}
-    <Q3/>
+    {/* <Q3/> */}
+    <Q4/>
     </>
   );
 }
