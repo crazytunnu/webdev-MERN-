211f6df48a26a0fafcb4a60e02daa68c1836128b
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-07-29 23:14:16 +0530

    creation of navbar

--- 6_React/reels/src/App.js
+++ 6_React/reels/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import AuthProvider from './Context/AuthProvider';
 import Signup from './component/Signup';
+import Main from './MaterialUI/main.js'
 function App() {
   return (
-    <AuthProvider>
-      <Signup/>
-    </AuthProvider>
+    <>
+
+    </>
   );
 }
 
