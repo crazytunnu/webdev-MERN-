64a0ec8327abd89847db70986220d460fbd4f43e
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-07-31 22:58:18 +0530

    started on july 31

---
+++ 6_React/reels/src/MaterialUI/Cards.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { FormHelperText } from '@material-ui/core';
+import Vid from './vid.mp4';
+import { height } from '@material-ui/system';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const useStyles = makeStyles({
+  root: {
+    margin:'0 auto',
+    width:'30%',
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  Vid:{
+      width:'100%',
+      height:'100vh'
+  }
+});
+
+export default function Cards() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <video className={classes.Vid}src={Vid} autoPlay muted></video>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          Learn More
+          <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- 6_React/reels/src/MaterialUI/main.js
+++ 6_React/reels/src/MaterialUI/main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './navbar';
 import Buttons from './buttons'
+import Cards from './Cards';
 export default function Main() {
     return (
         <div>
             <Navbar/>
             <Buttons/>
+            <Cards/>
         </div>
     )
 }
