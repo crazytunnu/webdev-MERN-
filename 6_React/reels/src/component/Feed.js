44d4cc26672f08fef31fd54c6b2e2aae7be40e6a
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-08-06 02:16:34 +0530

    completed code

--- 6_React/reels/src/component/Feed.js
+++ 6_React/reels/src/component/Feed.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useContext,useEffect,useState} from 'react'
+import { AuthContext } from '../Context/AuthProvider'
+import {database} from '../firebase'
+import { CircularProgress } from '@material-ui/core'
+import UploadFile from './UploadFile';
 import Header from './Header'
+import './Feed.css'
 export default function Feed() {
+    const {currentUser} =useContext(AuthContext);
+    const [userData,setUserData] = useState(null);
+    useEffect(()=>{
+        const unsub = database.users.doc(currentUser.uid).onSnapshot((doc)=>{
+            setUserData(doc.data())
+        })
+    },[currentUser])
     return (
         <>
-        <Header/>
+        { userData==null ? <CircularProgress />:<>
+        <Header userData={userData} />
+        <div style={{height:'9.5vh'}}/>
+        <div className='feed-container'>
+            <div className='center'>
+                <UploadFile userData={userData}/>
+            </div>
+        </div>
+
+        </>
+        }
         </>
     )
 }
