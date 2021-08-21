09863cc0d34ba352d32b405c37bfd945b2cf6841
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-08-22 01:06:16 +0530

    rootreducer changed to app reducer

--- 6_React/resume_builder/src/reducer/rootReducer.js
+++ 6_React/resume_builder/src/reducer/rootReducer.js
@@ -4,11 +4,21 @@ import documentReducer from './documentReducer'
 import educationReducer from './educationReducer'
 import {firebaseReducer} from 'react-redux-firebase'
 import {firestoreReducer} from 'redux-firestore'
-const rootReducer=combineReducers({
+import {authReducer} from './authReducer';
+import initialState from '../reducer/initialState.json'
+import { actionTypes, SIGN_OUT } from '../actions/actionTypes'
+const appReducer=combineReducers({
     firebase:firebaseReducer,
     firestore:firestoreReducer,
     contactSection:contactReducer,
     document:documentReducer,
-    educationSection:educationReducer
+    educationSection:educationReducer,
+    auth:authReducer
 })
+const rootReducer=(state=initialState,action)=>{
+    if(action.type===SIGN_OUT){
+        state=undefined
+    }
+    return appReducer(state,action);
+}
 export default rootReducer
