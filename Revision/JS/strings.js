6f43eafd3236aa4e3c9d017be4ad5bd779a2eb3c
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-08-27 02:56:42 +0530

    Strings done

---
+++ Revision/JS/strings.js
@@ -0,0 +1,55 @@
+// var string="The World";
+// console.log(string.length);
+
+
+// var sub1=string.slice(2);
+// console.log(sub1);
+
+
+// var sub2=string.slice(2,5);
+// console.log(sub2);
+
+
+
+
+//Replace
+
+// var str="The moon is dark";
+// var nstr=str.replace("moon","sky");
+// console.log(nstr);-------------------------The sky is dark
+//NOTE: replace only replaces the first occurance of the mentioned word;
+
+
+
+// // UpperCase
+// var str="aBc";
+// var nstr=str.toUpperCase();
+// console.log(nstr);
+
+
+// //LowerCase
+// var str="aBc";
+// var nstr=str.toLowerCase();
+// console.log(nstr);
+
+
+
+// //Trim
+// var str="          This is work                   "//spaces are useless
+// var nStr=str.trim();
+// console.log(nStr);//------------------------------"This is work"   all useless spaces are erased
+
+
+
+
+// //ASCII or charCodeAt
+// var str="ABD";
+// var x=str.charCodeAt(2);
+// console.log(x);
+
+
+
+// //Split
+// var str="Arshad Vishal Vivekanand";
+// var arr=str.split(" ");
+// console.log(arr);
