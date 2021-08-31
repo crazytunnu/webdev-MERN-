322af4e8485756314388b3d70e01a63aff16b7cc
ARSHAD HUSSAIN <crazytunnu@gmail.com>
2021-08-31 19:09:25 +0530

    this type 1

---
+++ Revision/JS/This/nonStrictNodejs.js
@@ -0,0 +1,36 @@
+// //Global
+// console.log(this);//----------represent empty object;
+
+
+// //Global function k andar
+// function A() {
+//     console.log(this);
+// }
+// A();//=======================global object
+
+
+
+
+//inside an object's immediate function
+// let obj={
+//     name:"Arshad",
+//     fun:function() {
+//         console.log(this);
+//     }
+// }
+// obj.fun();//immidiate function get object as this
+
+
+
+
+//non immidate function 
+let obj={
+        name:"Arshad",
+        fun:function() {
+            function fun2(){
+                console.log(this);
+            }
+            fun2();
+        }
+    }
+    obj.fun();//immidiate function get global object as this
